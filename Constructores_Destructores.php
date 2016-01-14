c27bd9e1bb33d170e454be51b6fda31e3cf227b5
Oscar <ospcg1@gmail.com>
2016-01-13 23:02:34 -0300

    Ejercicio de progroamacion orientada a objeto sobre el juego de la Loteria que elige numeros de forma aleatorias

---
+++ Constructores_Destructores.php
@@ -0,0 +1,43 @@
+<?php 
+class Loteria{
+
+	//Atributos
+	public $numero;
+	public $intentos;
+	public $resultado = false;
+
+	//Metodos
+	public function __construct($numero, $intentos){
+		$this->numero = $numero;
+		$this->intentos = $intentos;
+	}
+
+	public function sortear(){
+		$minimo = $this->numero / 2;
+		$maximo = $this->numero * 2;
+		for ($i=0; $i < $this->intentos; $i++) { 
+			$int = rand($minimo , $maximo);
+			self::intentos($int);
+		}
+	}
+
+	public function intentos($int){
+		if($int == $this->numero){
+			echo "<b>" .$int. " == " .$this->numero. "</b><br>";
+			$this->resultado = true;
+		}else{
+			echo $int . " != " . $this->numero . "<br>";
+		}
+	}
+
+	public function __destruct(){
+		if ($this->resultado){
+			echo "Felicidades, has ganado en ". $this->intentos ."intentos.";
+		}else{
+			echo "Lastima, has perdido en " .$this->intentos. "intentos";
+		}
+	}
+}
+$loteria = new Loteria(5, 10);
+$loteria->sortear();
+?>
